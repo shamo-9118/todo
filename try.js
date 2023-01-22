f8aa688b6f993cdbebe2e67543c18a06261c1b70
shamo-9118 <won9118391183@gmail.com>
2023-01-22 13:13:55 +0900

    気になったこと試す用のファイルを用意

---
+++ try.js
@@ -0,0 +1,25 @@
+const obj = {
+  a: "aaa",
+  b: "bbb",
+  c: "ccc",
+};
+
+const array = [];
+const messages = {
+  a: "hello",
+  b: "Ciao",
+  c: "こんにちは",
+};
+
+
+
+const testDiv = document.getElementById("test");
+// const fragment = document.createDocumentFragment();
+// const div1 = document.createElement("div");
+// const div2 = document.createElement("div");
+// const div3 = document.createElement("div");
+// array.push(div1, div2, div3);
+
+for (value in obj) {
+  obj[value];
+}
