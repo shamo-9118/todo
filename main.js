d74dda095c5a6d20bf7cc1789f940622dbbc1a04
shamo-9118 <won9118391183@gmail.com>
2023-01-24 06:04:56 +0900

    id登録時にテンプレートリテラルを使う

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ const handleSubmit = (event) => {
 
 const addTodo = (payload) => {
   const prevPayload = { ...payload };
-  prevPayload.id = "todo-" + (todoList.length + 1);
+  prevPayload.id = "todo-" + ([...todoList].length + 1);
   prevPayload.createdAt = new Date().toLocaleString();
   todoList.push(prevPayload);
   updateTodoList(prevPayload);
