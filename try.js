dc8106bcbc9c9dcfe2a3947a9c4232a22e9f3709
shamo-9118 <won9118391183@gmail.com>
2023-01-25 06:23:32 +0900

    test

--- try.js
+++ try.js
@@ -12,16 +12,13 @@ const messages = {
 
 const array = ["hey", "hello", "how are you", "whats up"];
 const text = "hey";
-const bool = array.find(message => {
-  return
-})
+const bool = array.find((message) => {
+  return;
+});
 const testDiv = document.getElementById("test");
-const button = document.createElement("button");
-button.setAttribute("onClick", `greeting(2)`);
-testDiv.appendChild(button);
-const greeting = (number) => {
-  alert(array[number]);
-};
+//スペースが入らない、、htmlの仕様？明日調べて実装する！
+const char = "     a        dd";
+testDiv.innerHTML = char;
 // const fragment = document.createDocumentFragment();
 // const div1 = document.createElement("div");
 // const div2 = document.createElement("div");
